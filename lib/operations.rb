b79aa51419e47b168b263e276fcf04b13c10c851
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-23 07:28:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-  speed < 60 && speed > 40 ? false: true
+  if speed < 60 && speed > 40
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 60 && speed > 40 ? false: true
 end
 	
 
